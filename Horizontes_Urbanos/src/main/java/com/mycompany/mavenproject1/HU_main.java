78b1d20a836653199477f1f2bc59d6a72996471e
OrtizS29 <santiago.ortizb06@gmail.com>
2024-10-13 16:52:44 -0500

    Cambio el nombre de la clase main.

--- Horizontes_Urbanos/src/main/java/com/mycompany/mavenproject1/HU_main.java
+++ Horizontes_Urbanos/src/main/java/com/mycompany/mavenproject1/HU_main.java
@@ -10,7 +10,7 @@ import Modelo.ConexionORACLE;
  *
  * @author SAlA-9
  */
-public class HU_main {
+    public class HU_main {
 
     public static void main(String[] args) {
         ConexionORACLE conn = new ConexionORACLE();
