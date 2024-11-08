99980e3d5633385c3b7ac7748b015e927bdd5128
juanxxho <jespinosa12@udi.edu.co>
2024-11-08 18:10:31 -0500

    a ver

--- Horizontes_Urbanos/src/main/java/VISTA/asesor/pagoSeleccionarClienteYVenta.java
+++ Horizontes_Urbanos/src/main/java/VISTA/asesor/pagoSeleccionarClienteYVenta.java
@@ -26,16 +26,127 @@ public class pagoSeleccionarClienteYVenta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tablaMostrarProyectos = new javax.swing.JTable();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        tablaMostrarProyectos1 = new javax.swing.JTable();
+        jButton3 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(910, 510));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        tablaMostrarProyectos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        tablaMostrarProyectos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tablaMostrarProyectosMouseClicked(evt);
+            }
+        });
+        jScrollPane3.setViewportView(tablaMostrarProyectos);
+
+        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 260, 480, 160));
+
+        tablaMostrarProyectos1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        tablaMostrarProyectos1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tablaMostrarProyectos1MouseClicked(evt);
+            }
+        });
+        jScrollPane4.setViewportView(tablaMostrarProyectos1);
+
+        getContentPane().add(jScrollPane4, new org.netbeans.lib.awtextra.AbsoluteConstraints(142, 80, 480, 160));
+
+        jButton3.setBackground(new java.awt.Color(49, 134, 181));
+        jButton3.setText("Siguiente");
+        jButton3.setBorder(null);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 450, 80, 30));
+
+        jButton2.setBackground(new java.awt.Color(49, 134, 181));
+        jButton2.setText("Menu");
+        jButton2.setBorder(null);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 450, 80, 30));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/pagoA.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void tablaMostrarProyectosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaMostrarProyectosMouseClicked
+
+        if(tablaMostrarProyectos.getRowCount() > 0){
+            if(tablaMostrarProyectos.getSelectedRow() != -1){
+
+                this.idProyectoSeleccionado = Integer.parseInt(String.valueOf(tablaMostrarProyectos.getValueAt(tablaMostrarProyectos.getSelectedRow(), 0)));
+
+                try {
+                    ActualizarTorre(idProyectoSeleccionado);
+                } catch (Exception ex) {
+                    Logger.getLogger(apartamentoSeleccionarProyecto.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        }
+
+    }//GEN-LAST:event_tablaMostrarProyectosMouseClicked
+
+    private void tablaMostrarProyectos1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaMostrarProyectos1MouseClicked
+
+        if(tablaMostrarProyectos.getRowCount() > 0){
+            if(tablaMostrarProyectos.getSelectedRow() != -1){
+
+                this.idProyectoSeleccionado = Integer.parseInt(String.valueOf(tablaMostrarProyectos.getValueAt(tablaMostrarProyectos.getSelectedRow(), 0)));
+
+                try {
+                    ActualizarTorre(idProyectoSeleccionado);
+                } catch (Exception ex) {
+                    Logger.getLogger(apartamentoSeleccionarProyecto.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        }
+
+    }//GEN-LAST:event_tablaMostrarProyectos1MouseClicked
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -73,6 +184,12 @@ public class pagoSeleccionarClienteYVenta extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTable tablaMostrarProyectos;
+    private javax.swing.JTable tablaMostrarProyectos1;
     // End of variables declaration//GEN-END:variables
 }
